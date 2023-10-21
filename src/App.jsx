c8d58c561fe6ccc6c825df4d10d48e3b146d1f36
Hussein <112550601+Husseinn-Essam@users.noreply.github.com>
2023-10-21 18:47:06 +0300

    theme change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <>
+    <div data-theme="" className="h-full">
       <Navbar />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="btn">hi?</button>
-    </>
+    </div>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
+  useEffect(() => {
+    themeChange(false);
+  }, []);
   return (
     <>
-      <div className="navbar bg-base-100">
+      <div className="navbar  bg-base-100 shadow-xl">
         <div className="navbar-start">Links here</div>
         <div className="navbar-center">
           <a className="btn btn-ghost normal-case text-xl">DB PROJECT</a>
         </div>
         <div className="navbar-end gap-2">
+          <button data-set-theme="dark" data-act-class="ACTIVECLASS">
+            <MoonIcon className="h-6 w-6 " />
+          </button>
+          <button data-set-theme="light" data-act-class="ACTIVECLASS">
+            <SunIcon className="h-6 w-6 " />
+          </button>
           <button className="btn">Log in</button>
           <button className="btn">Sign up</button>
         </div>
