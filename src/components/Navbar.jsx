77f7470c0982abefe887b0b356fe2502d421b370
Hussein <112550601+Husseinn-Essam@users.noreply.github.com>
2023-10-21 19:41:37 +0300

    ham menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,38 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { themeChange } from "theme-change";
-import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+import { MoonIcon, SunIcon, Bars3Icon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
   useEffect(() => {
     themeChange(false);
   }, []);
+  const [isOpen, setOpen] = useState(false);
+  const toggleOpen = () => {
+    setOpen(!isOpen);
+  };
+
   return (
     <>
-      <div className="navbar  bg-base-100 shadow-xl">
-        <div className="navbar-start">Links here</div>
+      <div className="navbar justify-center  bg-base-100 shadow-xl">
+        <div className="hidden navbar-start md:flex">links</div>
         <div className="navbar-center">
           <a className="btn btn-ghost normal-case text-xl">DB PROJECT</a>
         </div>
-        <div className="navbar-end gap-2">
+        <div className="navbar-end gap-2   ">
           <button data-set-theme="dark" data-act-class="ACTIVECLASS">
             <MoonIcon className="h-6 w-6 " />
           </button>
           <button data-set-theme="light" data-act-class="ACTIVECLASS">
             <SunIcon className="h-6 w-6 " />
           </button>
-          <button className="btn">Log in</button>
-          <button className="btn">Sign up</button>
+          <div className="hidden md:flex">
+            <button className="btn">Log in</button>
+            <button className="btn">Sign up</button>
+          </div>
+          <button className="xl" onClick={toggleOpen}>
+            <Bars3Icon className="h-10 w-10 md:hidden " />
+          </button>
         </div>
       </div>
     </>
